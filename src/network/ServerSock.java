977ab3098116e9f15e833996a24d6ef458c50291
Colin Murphy <cjmurphy@siu.edu>
2012-08-05 12:33:13 -0500

    moved cast outside switch

--- src/network/ServerSock.java
+++ src/network/ServerSock.java
@@ -8,6 +8,7 @@ import java.net.SocketException;
 
 public class ServerSock extends Network {
     private Server server;
+    private InetSocketAddress address;
 
     public ServerSock(Server s) {
         server = s;
@@ -21,19 +22,19 @@ public class ServerSock extends Network {
 
     void process() {
         stateBuffer.clear();
+        address = (InetSocketAddress) statePacket.getSocketAddress();
         switch (stateBuffer.get()) {
             case CONNECT:
                 System.err.println("connection packet received");
-                server.addClient((InetSocketAddress) statePacket.getSocketAddress());
+                server.addClient(address);
                 break;
             case UPDATE:
                 //System.err.println("update packet received");
-                server.updateClient((InetSocketAddress) statePacket.getSocketAddress(),
-                        stateBuffer.getInt(), stateBuffer.getInt());
+                server.updateClient(address, stateBuffer.getInt(), stateBuffer.getInt());
                 break;
             case DISCONNECT:
                 System.err.println("disconnection packet received");
-                server.removeClient((InetSocketAddress) statePacket.getSocketAddress());
+                server.removeClient(address);
                 break;
             default:
                 System.err.println("unknown packet received");
